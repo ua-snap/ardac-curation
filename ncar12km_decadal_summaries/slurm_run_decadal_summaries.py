6f6e7e75cee0503ec7838ad5a63a63711dc5ad1e
Charlie Parr <cparr4@atlas10.snap.uaf.edu>
2023-02-17 20:52:47 -0900

    ignore unused imports and client object

--- ncar12km_decadal_summaries/slurm_run_decadal_summaries.py
+++ ncar12km_decadal_summaries/slurm_run_decadal_summaries.py
@@ -1,26 +1,7 @@
 import argparse
-import glob
-import xarray as xr
-import numpy as np
-import rasterio as rio
-import pickle
 
-# import logging
-import warnings
-from pathlib import Path
-from wrf_raster_profile import create_wrf_raster_profile
 from generate_decadal_summaries import create_decadal_averages
-import dask.distributed
-from dask_jobqueue import SLURMCluster
-from config import (
-    mo_names,
-    months,
-    unit_di,
-    summary_di,
-    variable_di,
-    precision_di,
-    target_dirs,
-)
+from config import target_dirs
 
 
 if __name__ == "__main__":
@@ -29,7 +10,6 @@ if __name__ == "__main__":
     parser.add_argument("output_dir", help="directory to save output GeoTIFF files")
     args = parser.parse_args()
 
-    client = dask.distributed.Client(n_workers=32)
+
     for target in target_dirs:
         create_decadal_averages(target, output_dir=args.output_dir, dry_run=False)
-    client.close()
