51fb0778dd8cef0fccd91a0265958312c8be8d9b
Charlie Parr <cparr4@atlas18.snap.uaf.edu>
2024-04-12 06:56:00 -0800

    distinguish true zero values from no data values

--- degree_days/compute_degree_days.py
+++ degree_days/compute_degree_days.py
@@ -27,42 +27,51 @@ def summarize_year_dd(temp_ds, temp_threshold, count_days_below_threshold):
     """
 
     if count_days_below_threshold:
-        # Use a bool variable (below) to specify that the DD
+        #  Use a bool variable (below) to specify that the DD
         #  below the threshold should be counted. This
         #  will be the case for heating DD, DD below 0, and freezing index
-        degree_day_arr = temp_threshold - temp_ds.tavg_F
+        degree_delta_arr = temp_threshold - temp_ds.tavg_F
     else:
         # Otherwise, count degree days above some threshold
-        degree_day_arr = temp_ds.tavg_F - temp_threshold
+        degree_delta_arr = temp_ds.tavg_F - temp_threshold
 
-    # replace negative values with 0
-    degree_day_arr = degree_day_arr.where(degree_day_arr >= 0, 0)
+    # replace negative values with 0 prior to summing, but retain initial `np.nan` no data values
+    degree_delta_arr = degree_delta_arr.where(
+        (degree_delta_arr >= 0) | np.isnan(degree_delta_arr), 0
+    )
+
     # total sum of degree days for the year
-    degree_day_arr = np.round(degree_day_arr.sum(axis=0))
+    sum_arr = np.nansum(degree_delta_arr, axis=0)
+    # Replace sums that are 0 with `nan` if all values in the original stack were `nan`
+    sum_arr = np.where(np.all(np.isnan(degree_delta_arr), axis=0), np.nan, sum_arr)
 
+    # Create a new xarray.DataArray using sum_arr, and copy over the 'y' and 'x' coordinates and attributes
+    degree_day_arr = xr.DataArray(
+        np.round(sum_arr),
+        coords={"y": temp_ds.tavg_F.coords["y"], "x": temp_ds.tavg_F.coords["x"]},
+        dims=["y", "x"],
+        attrs=temp_ds.tavg_F.attrs,
+    )
+
+    degree_day_arr = degree_day_arr.where(~np.isnan(degree_day_arr), -9999)
     return degree_day_arr
 
 
 def compute_cumulative_freezing_index(temp_ds):
     air_freezing_index = summarize_year_dd(temp_ds, 32, True)
-    # 0 values will be no data
-    air_freezing_index = air_freezing_index.where(air_freezing_index != 0, -9999)
     return air_freezing_index
 
 
 def compute_cumulative_heating_degree_days(temp_ds):
     heating_degree_days = summarize_year_dd(temp_ds, 65, True)
-    heating_degree_days = heating_degree_days.where(heating_degree_days != 0, -9999)
     return heating_degree_days
 
 
 def compute_cumulative_degree_days_below_0F(temp_ds):
     degree_days_below0F = summarize_year_dd(temp_ds, 0, True)
-    degree_days_below0F = degree_days_below0F.where(degree_days_below0F != 0, -9999)
     return degree_days_below0F
 
 
 def compute_cumulative_thawing_index(temp_ds):
     air_thawing_index = summarize_year_dd(temp_ds, 32, False)
-    air_thawing_index = air_thawing_index.where(air_thawing_index != 0, -9999)
     return air_thawing_index
